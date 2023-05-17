1f60bf5506c20d7f3ca69754117006393c9de2c5
AnaKelySantos <anasa.prof@gmail.com>
2023-05-17 11:03:58 -0300

    aula 11 e pratica

---
+++ aula11/exe001.js
@@ -0,0 +1,13 @@
+//console.log('o console funcionou perfeitamente')
+
+// condição composta
+var vel = 80.5
+
+if(vel>65){
+    console.log(`cuidado sua velocidade está acima da permitida  diminua de ${vel} para 65`)
+}else{
+    console.log(`velocidade permitida ${vel}`)
+}
+
+//só um teste
+console.log('oi')
---
+++ aula11/exe002.js
@@ -0,0 +1,10 @@
+var pais = 'EUA'
+
+console.log(`veivendo em ${pais}`)
+
+if (pais == ' Brasil'){
+    console.log('brasileiro')
+}else{
+
+    console.log('estreangeiro')
+}
