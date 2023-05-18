9b21ab07857d846282be635db686b8aa8c676bf8
AnaKelySantos <anasa.prof@gmail.com>
2023-05-18 09:19:21 -0300

    aula12

---
+++ aula12/exe003.js
@@ -0,0 +1,35 @@
+//condição multipla
+
+var idadeMeses = 9
+switch (idadeMeses) {
+    case 1: 
+        console.log(`4 semanas`)
+        break;
+        case 2: 
+        console.log(`8 semanas`)
+        break;
+        case 3: 
+        console.log(`12 semanas`)
+        break;
+        case 4: 
+        console.log(`16 semanas`)
+        break;
+        case 5: 
+        console.log(`20 semanas`)
+        break;
+        case 6: 
+        console.log(`24 semanas`)
+        break;
+        case 7: 
+        console.log(`28 semanas`)
+        break;
+        case 8: 
+        console.log(`32 semanas`)
+        break;
+        case 9: 
+        console.log(`36 semanas`)
+        break;
+    default:
+        console.log(`nasceu`)
+        break;
+}
