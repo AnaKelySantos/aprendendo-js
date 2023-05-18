357c807d8b5ee58e1dc213d9557b5bec3637cb50
AnaKelySantos <anasa.prof@gmail.com>
2023-05-18 13:05:36 -0300

    cor do fundo

--- pratica1/script.js
+++ pratica1/script.js
@@ -4,18 +4,21 @@ function carregar() {
     var data = new Date()
     var hora = data.getHours()
 
-    texto.innerHTML = `Agora são ${hora} hora.`
+    texto.innerHTML = `Agora são ${hora} horas.`
     if (hora>4 && hora<12) {
         texto.innerHTML += ` Bom dia!`
         texto.innerHTML += `<img src="exercicio/fotor_2023-5-18_11_49_53-removebg-preview.png" alt="foto do dia">`
+        document.body.style.background = '#c8a2c8'
     } else {
         if (hora<18) {
             texto.innerHTML += ` Boa tarde!`
         texto.innerHTML += `<img src="exercicio/fotor_2023-5-18_11_51_30-removebg-preview.png">`
+        document.body.style.background = '#f08080'
         } else {
             
             texto.innerHTML += ` Boa noite!`
         texto.innerHTML += `<img src="exercicio/fotor_2023-5-18_11_52_27-removebg-preview.png">`
+        document.body.style.background = '#c8a2c8'
             
         }
         
