9d72f20ad43575a65c7302e8165086cc06e681ad
AnaKelySantos <anasa.prof@gmail.com>
2023-05-19 16:33:39 -0300

    exer for

---
+++ pratica3/script.js
@@ -0,0 +1,15 @@
+function contar() {
+    
+var num1 = window.document.getElementById(`txtum`)
+var num2 = window.document.getElementById(`txtdois`)
+var res = window.document.querySelector(`div#dois`)
+
+for ( var c = num1; c >= num2; c++) {
+    
+res.innerHTML =`Contando: <br>`
+res.innerHTML =`${c} `
+res.innerHTML =` `
+
+
+}
+}
