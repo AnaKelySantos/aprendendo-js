7b5eb04b3644028f22aecdee99520e62ca95afed
AnaKelySantos <anasa.prof@gmail.com>
2023-05-19 14:54:37 -0300

    do while

---
+++ aula13.2/script.js
@@ -0,0 +1,17 @@
+function comerPizza() {
+    
+   
+    var texto = window.document.querySelector(`div#dois`)
+   
+    c = 1
+ 
+   
+      do{
+
+        texto.innerHTML += `${c}<br>`
+        c++
+}while (c<=6)
+      }
+        
+
+
