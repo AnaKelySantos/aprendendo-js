2f30b372a524e2bd266f445d5886c652ee797329
AnaKelySantos <anasa.prof@gmail.com>
2023-05-19 10:23:19 -0300

    pratica 2

--- pratica2/script.js
+++ pratica2/script.js
@@ -1,25 +1,55 @@
 function verificar() {
 
-    
+
     var data = new Date()
     var ano = data.getFullYear()
-   var erro = document.getElementById(`txtano`)
-   var fAno = Number(erro.value)
-   var res = window.document.querySelector('div#dois')
-   
-    
-   if (fAno == 0 || fAno > ano ) {
-    window.alert(`deu errado`)
-   }else{
-            var sex = document.getElementsByName(`radsex`)[1]
-            var idade = ano - fAno
-            if () {
-                
+    var ponto = document.getElementById(`txtano`)
+    var fAno = Number(ponto.value)
+    var res = window.document.querySelector('div#dois')
+
+
+    if (fAno == 0 || fAno > ano) {
+        window.alert(`deu errado`)
+    } else {
+        var fsex = document.getElementsByName(`radsex`)
+        var idade = ano - fAno
+        var genero = ''
+        if (fsex[0].checked) {
+            var genero = 'masculino'
+
+            if (idade < 13) {
+                res.innerHTML = `criança de ${idade} anos do sexo  ${genero}.`
+                res.innerHTML += `<img src="fotos/meninocrianca.jpg">`
+            } else if (idade < 18) {
+                res.innerHTML = `Jovem de ${idade} anos do sexo  ${genero}.`
+                res.innerHTML += `<img src="fotos/homemjovem.jpg">`
+            } else if (idade < 64) {
+                res.innerHTML = `Adulto de ${idade} anos do sexo  ${genero}.`
+                res.innerHTML += `<img src="fotos/homemadulto.jpg">`
             } else {
-                
+                res.innerHTML = `Idoso de ${idade} anos do sexo  ${genero}.`
+                res.innerHTML += `<img src="fotos/homemvelho.jpg">`
             }
-    res.innerHTML = `criança de ${idade} anos do sexo .`
-            
-        }  
+
+
+        } else {
+            var genero = 'Feminino'
+            if (idade < 13) {
+                res.innerHTML = `criança de ${idade} anos do sexo  ${genero}.`
+                res.innerHTML += `<img src="fotos/mulhercrianca.jpg">`
+                
+            } else if (idade < 18) {
+                res.innerHTML = `Jovem de ${idade} anos do sexo  ${genero}.`
+                res.innerHTML += `<img src="fotos/mulherjovem.jpg">`
+            } else if (idade < 64) {
+                res.innerHTML = `Adulto de ${idade} anos do sexo  ${genero}.`
+                res.innerHTML += `<img src="fotos/mulheradulta.jpg">`
+            } else {
+                res.innerHTML = `Idosa de ${idade} anos do sexo  ${genero}.`
+                res.innerHTML += `<img src="fotos/mulhervelha.jpg">`
+            }
+
+        }
     }
-    
+}
+
