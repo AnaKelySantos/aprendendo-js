14b2b91c30f78f7fb0c2f0bbce791c3530ed7005
AnaKelySantos <anasa.prof@gmail.com>
2023-05-19 16:56:10 -0300

    forexisucce

--- pratica3/script.js
+++ pratica3/script.js
@@ -1,15 +1,22 @@
 function contar() {
     
 var num1 = window.document.getElementById(`txtum`)
+var um = Number(num1.value)
 var num2 = window.document.getElementById(`txtdois`)
+var dois = Number(num2.value)
 var res = window.document.querySelector(`div#dois`)
-
-for ( var c = num1; c >= num2; c++) {
-    
 res.innerHTML =`Contando: <br>`
-res.innerHTML =`${c} `
-res.innerHTML =`<img src="/homemjovem.jpg">` 
+
+for ( var c = um; c <= dois; c++) {
+
+    
+    
+
+res.innerHTML +=`${c} <img src="icones/direita.png">` 
+
 
 
 }
+res.innerHTML +=` <img src="icones/bandeira.png">` 
+
 }
