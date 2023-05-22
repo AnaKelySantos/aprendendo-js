22d1839766821298c44f8499aa30cb8a817fd657
AnaKelySantos <anasa.prof@gmail.com>
2023-05-22 15:05:49 -0300

    atu

---
+++ ambiente de teste/array.js
@@ -0,0 +1,24 @@
+var a = [`safira`,`manuela`,`lidiane`]
+
+let b = [5,2,3]
+
+//console.log(`${a[2]}`)
+
+b[3] = `malfada`
+
+//console.log(b)
+a.push(`mila`)
+//console.log(a)
+
+
+a.length
+a.sort()
+//console.log(b)
+//console.log(a.length)
+for (c = 0; c <= 4; c++) {
+    var valores = a[c];
+    console.log(valores)
+    
+}
+//console.log(`${a[2]}`)
+
