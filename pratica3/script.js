f81a59152899dee7c6574551fdd2354ecc993ced
AnaKelySantos <anasa.prof@gmail.com>
2023-05-22 10:52:10 -0300

    completando requisitos pratica 3

--- pratica3/script.js
+++ pratica3/script.js
@@ -7,18 +7,53 @@ var dois = Number(num2.value)
 var num3 = window.document.getElementById(`txttres`)
 var tres = Number(num3.value)
 var res = window.document.querySelector(`div#dois`)
-res.innerHTML =`Contando: <br>`
 
-for ( var c = um; c <= dois; c = c+tres) {
+if (um == 0|| dois == 0) {
+    
+window.alert(`erro!`)
 
+} else {
+
+    res.innerHTML =`Contando: <br>`
+
+
+    if (tres==0) {
+   
+        window.alert('numero invalido, considero 1!')
+        for ( var c = um; c <= dois; c++) {
+    
+        
+        
+    
+            res.innerHTML +=`<img src="icones/direita.png"> ${c} ` 
+            
+            
+            
+            }
     
     
+    } else {
+        res.innerHTML =`Contando: <br>`
 
-res.innerHTML +=`${c} <img src="icones/direita.png">` 
-
-
+    
+        for ( var c = um; c <= dois; c = c+tres) {
+    
+        
+        
+    
+            res.innerHTML +=`<img src="icones/direita.png"> ${c} ` 
+            
+            
+            
+            }
+    
+    }
+    
+    
+    res.innerHTML +=` <img src="icones/bandeira.png">` 
+    
 
 }
-res.innerHTML +=` <img src="icones/bandeira.png">` 
+
 
 }
