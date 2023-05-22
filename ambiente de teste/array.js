870b51315b90ca45b496a15bd563bfaa2f27a46d
AnaKelySantos <anasa.prof@gmail.com>
2023-05-22 15:17:54 -0300

    atu

--- ambiente de teste/array.js
+++ ambiente de teste/array.js
@@ -15,10 +15,14 @@ a.length
 a.sort()
 //console.log(b)
 //console.log(a.length)
-for (let c = 0; c <= b.length; c++) {
+//for (let c = 0; c <= b.length; c++) {
     //var valores = a[c];
     //console.log(valores)
-    console.log(b[c])
-}
+  //  console.log(b[c])
+//}
 //console.log(`${a[2]}`)
 
+for (let c in a) {
+
+    console.log(a[c])
+}
