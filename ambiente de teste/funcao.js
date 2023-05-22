a61a97f54c4da0e8f74e9e9965ccbbac4a853f8c
AnaKelySantos <anasa.prof@gmail.com>
2023-05-22 16:09:19 -0300

    atu

--- ambiente de teste/funcao.js
+++ ambiente de teste/funcao.js
@@ -13,10 +13,20 @@ function parimp(n) {
 //let res = parimp(4)
 //console.log(res)
 
-function parimp2(n1, n2) {
+function parimp2(n1=0, n2=0) {
          
         return n1 + n2
     
 }
-let res = parimp2(4,3)
-console.log(res)
+//let res = parimp2(4,3)
+//console.log(res)
+
+//let res = parimp2(3)
+//console.log(res)
+var v = function(x) {
+
+    return x*2
+    
+}
+
+console.log(v(5))
