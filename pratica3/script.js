5a0284121f45344c4287a87c93076a56dbf591a8
AnaKelySantos <anasa.prof@gmail.com>
2023-05-22 09:20:49 -0300

    atualiza

--- pratica3/script.js
+++ pratica3/script.js
@@ -4,10 +4,12 @@ var num1 = window.document.getElementById(`txtum`)
 var um = Number(num1.value)
 var num2 = window.document.getElementById(`txtdois`)
 var dois = Number(num2.value)
+var num3 = window.document.getElementById(`txttres`)
+var tres = Number(num3.value)
 var res = window.document.querySelector(`div#dois`)
 res.innerHTML =`Contando: <br>`
 
-for ( var c = um; c <= dois; c++) {
+for ( var c = um; c <= dois; c = c+tres) {
 
     
     
